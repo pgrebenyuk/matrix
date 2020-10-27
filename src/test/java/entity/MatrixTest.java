f99f167356b5c122240fb8535fe1fc8b19ca846c
rustam.vierdiyev <rustam.vierdiyev@gmail.com>
2020-10-27 18:37:48 +0200

    Added review.

--- src/test/java/entity/MatrixTest.java
+++ src/test/java/entity/MatrixTest.java
@@ -1,15 +1,38 @@
 package entity;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static entity.Atom.*;
 
 public class MatrixTest {
+    //коми ставлять в тій ж стрічці
+//    private Atom[][] array= {{HOLE, BLUE, BLACK},
+//            {HOLE, BLUE, HOLE},
+//            {BLACK, BLUE, BLACK}};
     private Atom[][] array= {{HOLE, BLUE, BLACK}
                             ,{HOLE, BLUE, HOLE}
                             ,{BLACK, BLUE, BLACK}};
     private Matrix matrix = new Matrix(array);
+
+    /*
+    ініціалізувати можна так
+    цей метод буде визиватись перед кожним новим класом теста
+    тоді змінної array тобі взагалі не треба буде
+
+    @Before
+    public void before() {
+        Atom[][] array= {{HOLE, BLUE, BLACK},
+            {HOLE, BLUE, HOLE},
+            {BLACK, BLUE, BLACK}};
+
+        this.matrix = new Matrix(array);
+    }
+
+    ще є @BeforeClass
+    визветься один раз перед всіма тестами
+    */
 
     @Test
     public void getMatrix() {
--- src/test/java/manager/MatrixManagerImplTest.java
+++ src/test/java/manager/MatrixManagerImplTest.java
@@ -5,7 +5,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MatrixManagerImplTest {
+    //цього менеджера мжна лишити тут як поле
     private MatrixManager matrixManager = new MatrixManagerImpl();
+
+    //а от матрицю краще вже ініціалізувати в конкретному тесті кожен раз і забрати звідси
     private Matrix matrix = matrixManager.initializationMatrix(5);
 
     @Test
@@ -20,7 +23,18 @@ public class MatrixManagerImplTest {
 
     @Test
     public void initializationMatrixMinus() {
+        //краще зроби, щоб якщо мінус, то викидало IllegalArgumentException
         Matrix matrixMinus = matrixManager.initializationMatrix(-5);
+        //пробіл чогось в параметрах вкінці
         Assert.assertEquals(2, matrixMinus.getMatrix().length );
     }
+
+    //метод прінта ти не протестиш, бо по суті він нічого не робить
+
+    //а от мікс метод зможеш протестити
+    //для цього треба використати мокіто і замокати рандом, щоб дав тобі те, що треба
+    //а для того, щоб його замокати, то його треба передавати в метод, чи клас, а не створювати прям там
+
+    //ще в тебе не протестований клас UserInputManager
+
 }
